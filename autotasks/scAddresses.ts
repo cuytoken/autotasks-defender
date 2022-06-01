40ee5aa19da58dc3fb79d0ead25f2d820364a27f
Lee Marreros <lee.marreros@pucp.pe>
2022-06-01 10:27:41 -0500

    Updates SC addresses

--- autotasks/scAddresses.ts
+++ autotasks/scAddresses.ts
@@ -1,3 +1,3 @@
-export var tatacuyAddress = "0xD6cDFb590364E478AB3151Edc0180ebf82Bb456F";
-export var wiracochaAddress = "0x8049f248b7645CAbe552aED85fD897E79aAF7080";
+export var tatacuyAddress = "0xAd44dA390Cd081316a2aB756E6653b2083ac06F4";
+export var wiracochaAddress = "0xf08E3E310562a355E264420917dcCD352b9005b9";
 export var busdAddress = "0x8F1C7AAf8EC93A500657aEc7C030d392fd4cAA13";
